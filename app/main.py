dc44796248fb58bf17c86d80ccb5b9393ef4f3a8
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-22 22:53:28 +0530

    good-prompt

--- app/main.py
+++ app/main.py
@@ -3,6 +3,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#  prompt-6-things
+#  1) role - role of agent (ex-1-you are a engineer responsible for reviewing code)(good) (ex-2-you are a genius enginner(bad)(should be genius))
+#  2) task - what is work to the agent (classification of the email in different categories), (short summary of email),etc
+#  3) constraints - boundary - give answer to give given things.
+#  4) output-format - one word answer(ex. category classification), (100-150 words summary of email), (json)
+#  5) zero/oneshot/example - give example to agent for more clarity to the ai
+#  6) fallback - unralted thing/issue for the specific app to is unreleted return other/something-else
+
+# good-prompt = "
+# #ROLE: you are engineer who is responsible for reviewing code 
+# #TASK: classify email into i category
+# #CONSTRAINTS: you have to classify this email into these categories = [categpries]
+# #OUTPUT FORMAT: you answer should be in one word and onw word should be one of categories give to you in constraints
+# #Example: for instance - if user was charged more than the laptop price, it's a billing issue
+# #FALLBACK: if the issue unreleted to any of the categories mentioned in constrainst then the anwer should be other
+# "
+
 @app.get("/")
 def root():
     return getSummaryOfEmail()
