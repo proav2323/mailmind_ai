5ee1be4d1c5ccde85fb6d570608040c26a5ddf23
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-27 20:51:04 +0530

    check_if_50-60 emails gethandeld_on_at_time

--- app/main.py
+++ app/main.py
@@ -51,8 +51,8 @@ def root():
 @app.post("/email")
 async def email(emailData: emailItem):
     emails = loads(emailData.data)
-    # tasks = [processEmails(email) for email in emails]
-    # result = await asyncio.gather(*tasks)
+    tasks = [processEmails(email) for email in emails]
+    result = await asyncio.gather(*tasks)
     data = await processEmails(email=emails[0])
 
     return {"data": data}
