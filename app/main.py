d6048af77633986f4691345ed7b7ff033ddb8fd7
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-30 18:52:35 +0530

    get-new-prioiriteis

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import app.utils.ai as ai # production
 # import utils.ai as ai  # developemtn only
 from pydantic import BaseModel
 from json import loads
-from aiolimiter import AsyncLimiter
 
 app = FastAPI()
 
@@ -31,8 +30,6 @@ app = FastAPI()
 # 3) get deadline if any (if deadline -> place event in user calaender(react tool)) -> done diealine (not calender tool(later))
 # 4) get subject -> done
 # 5) get priority -> done
-rate_limiter = AsyncLimiter(max_rate=10, time_period=60)
-concurrency_semaphore = asyncio.Semaphore(3)
 
 def chunk_list(lst, size):
     return [lst[i:i + size] for i in range(0, len(lst), size)]
@@ -55,9 +52,18 @@ class emailItem(BaseModel):
 
 async def processEmails(email):
     try:
-        await asyncio.sleep(1.5) 
         data = await ai.getAiEmailResponse(email['body'], email['categories'])
         returnData = {"category": data.category, "id":  email['myGivenId'], "summary": data.summary, "deadline": data.deadline, "subject": data.subject, "priority": data.priority}
+                
+        return returnData
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=e)
+
+async def processEmailsPrioirty(email):
+    try:
+        data = await ai.getEmailsPriority(email['body'])
+        returnData = {"id":  email['myGivenId'],"priority": data.priority}
                 
         return returnData
     except Exception as e:
@@ -87,3 +93,21 @@ async def email(emailData: emailItem):
             
     return {"data": results}
 
+@app.post("/priority")
+async def emailPriority(emailData: emailItem):
+    emails = loads(emailData.data)
+    email_batches = chunk_list(emails, 10)
+    results = []
+    
+    for index, batch in enumerate(email_batches):
+        print(f"Executing batch {index + 1}/{len(email_batches)}...")
+        
+        batch_tasks = [processEmailsPrioirty(e) for e in batch]
+        batch_results = await asyncio.gather(*batch_tasks)
+        results.extend(batch_results)
+        
+        if index < len(email_batches) - 1:
+            print(f"Batch {index + 1} done. Sleeping 65 seconds to completely reset Google quota...")
+            await asyncio.sleep(65)
+            
+    return {"data": results}
